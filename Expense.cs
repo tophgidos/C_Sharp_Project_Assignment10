58400212fe299eb9779e7976a5b0b2a93358ecf5
Topher <36679442+tophgidos@users.noreply.github.com>
2018-06-07 14:52:13 -0700

    Initial Commit

---
+++ Expense.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Employees
+{
+    public enum ExpenseCategory { Travel, Meals, Lodging, Conference, Misc }
+
+    [Serializable]
+    public class Expense
+    {
+        #region Data members / properties
+        public DateTime Date { get; set; } = DateTime.Today;
+        public ExpenseCategory Category { get; set; } = ExpenseCategory.Travel;
+        public string Description { get; set; } = "";
+        public double Amount { get; set; } = 0.0;
+        #endregion
+
+        #region Constructors
+        public Expense() { }
+
+        public Expense(DateTime expDate, ExpenseCategory category, string description, double amount)
+        {
+            Date = expDate;
+            Category = category;
+            Description = description;
+            Amount = amount;
+        }
+        #endregion
+    }
+}
