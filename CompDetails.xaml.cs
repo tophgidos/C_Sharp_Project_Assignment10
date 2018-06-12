1bfa2e25b71c1beda2d705a60ce39d2ff66ed1d0
tophgidos <36679442+tophgidos@users.noreply.github.com>
2018-06-12 14:39:32 -0700

    Refresh Logic
    Added refresh logic to Bonus and promotion buttons.

--- CompDetails.xaml.cs
+++ CompDetails.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Text.RegularExpressions;
 
 namespace Employees
 {
@@ -52,12 +53,45 @@ namespace Employees
 
             float b = float.Parse(BonusInput.Text);
             this.emp.GiveBonus(b);
+            //refresh page
+            Refresh_Page();
+        }
+
+        
+        private void Bonus_CanExecute(object sender, System.Windows.RoutedEventArgs e)
+        {
+            
+            
         }
 
         private void Promotion_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             this.emp.GivePromotion();
+
+            //refresh page
+            Refresh_Page();
             
+        }
+
+
+        private void Refresh_Page()
+        {
+            object obj = this.DataContext;
+            this.DataContext = null;
+            this.DataContext = obj;
+
+            string name1 = "";
+            string value1 = "";
+            string name2 = "";
+            string value2 = "";
+
+            emp.SpareDetailProp1(ref name1, ref value1);
+            emp.SpareDetailProp2(ref name2, ref value2);
+
+            SpareProp1Name.Content = name1;
+            SpareProp1Value.Content = value1;
+            SpareProp2Name.Content = name2;
+            SpareProp2Value.Content = value2;
         }
     }
 }
